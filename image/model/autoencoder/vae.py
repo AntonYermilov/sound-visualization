07e509747da5e7d450c9fb01682cc923eb594751
Anton Yermilov <anton.yermilov@gmail.com>
2019-12-18 15:22:22 +0300

    fixed vae

--- image/model/autoencoder/vae.py
+++ image/model/autoencoder/vae.py
@@ -1,25 +1,31 @@
 import torch
 import torch.nn as nn
 from enum import Enum
+import torch.nn.functional as F
+
 
 class EncoderState(Enum):
     TRAIN = 1
     EVAL = 2
     PREDICT = 3
 
+
 class VAE(nn.Module):
-    def __init__(self, size=784, hidden_size=20, num_class=10):
+    def __init__(self, channels=1, height=28, width=28, hidden_size=20, num_class=10, device='cpu'):
         super(VAE, self).__init__()
-        self.size = size
+        self.channels = channels
+        self.width = width
+        self.height = height
         self.hidden_size = hidden_size
         self.num_class = num_class
         self.state = EncoderState.TRAIN
+        self.device = device
 
-        self.fc1 = nn.Linear(self.size, 400)
+        self.fc1 = nn.Linear(self.width * self.height, 400)
         self.fc21 = nn.Linear(400, self.hidden_size)
         self.fc22 = nn.Linear(400, self.hidden_size)
         self.fc3 = nn.Linear(self.hidden_size, 400)
-        self.fc4 = nn.Linear(400, self.size)
+        self.fc4 = nn.Linear(400, self.width * self.height)
 
         self.classifier = nn.Sequential(
             nn.Linear(self.hidden_size, self.hidden_size),
@@ -40,16 +46,19 @@ class VAE(nn.Module):
 
     def forward(self, x):
         if self.state == EncoderState.EVAL:
-            return self.decode(x)
+            n, _ = x.shape
+            return self.decode(x).reshape(n, self.channels, self.height, self.width)
 
         if self.state == EncoderState.PREDICT:
             return self.classifier(x)
 
         mu, logvar = self.encode(x)
-        z = mu + torch.sqrt(logvar.exp()) * torch.randn(logvar.shape).to(device)
+        z = mu + torch.sqrt(logvar.exp()) * torch.randn(logvar.shape).to(self.device)
+
         logits = self.classifier(z)
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
+
         reconstruction = self.decode(mu + eps * std).view_as(x)
         return logits, reconstruction
 
